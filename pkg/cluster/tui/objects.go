547a215f7b37137fc601e96fbd32e4d9e009fad3
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2024-02-17 04:11:32 +0200

    Refresh list of migrated objects in interactive mode

--- pkg/cluster/tui/objects.go
+++ pkg/cluster/tui/objects.go
@@ -7,7 +7,10 @@ import (
 
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/progress"
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -19,6 +22,8 @@ type Objects struct {
 	mode     mode
 	list     list.Model
 	quitting bool
+	spinner  spinner.Model
+	progress progress.Model
 }
 
 // Init run any intial IO on program start
@@ -29,7 +34,14 @@ func (m Objects) Init() tea.Cmd {
 func InitObjects(i item, p *tea.Program) *Objects {
 	var title string
 	items := []list.Item{}
-	m := Objects{mode: nav}
+	pr := progress.New(
+		progress.WithDefaultGradient(),
+		progress.WithWidth(40),
+		progress.WithoutPercentage(),
+	)
+	s := spinner.New()
+	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("63"))
+	m := Objects{mode: nav, progress: pr, spinner: s}
 	switch i.objType {
 	case "project":
 		// in case of individual project then we will list namespaces and prtbs
@@ -126,10 +138,15 @@ func (m Objects) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				return entry.Update(constants.WindowSize)
 			}
 		case key.Matches(msg, constants.Keymap.Migrate):
+			var err error
 			item := m.list.SelectedItem().(item)
-			if err := migrateObject(context.Background(), item); err != nil {
+			if _, err = migrateObject(context.Background(), item); err != nil {
 				panic(err)
 			}
+			item.objType = item.objType + "s"
+			entry := InitObjects(item, constants.P)
+			return entry.Update(constants.WindowSize)
+
 		default:
 			m.list, cmd = m.list.Update(msg)
 		}
@@ -157,65 +174,74 @@ func status(name string, migrated, diff bool) (string, constants.MigrationStatus
 	return name + " " + constants.WrongMark, constants.NotMigratedStatus
 }
 
-func migrateObject(ctx context.Context, i item) error {
+func migrateObject(ctx context.Context, i item) (tea.Msg, error) {
+	var msg string
 	switch i.objType {
 	case constants.ProjectType:
 		if i.status == constants.NotMigratedStatus {
 			p := i.obj.(*cluster.Project)
 			p.Mutate(constants.TC)
 			if err := constants.Lclient.Projects.Create(ctx, constants.TC.Obj.Name, p.Obj, nil, v1.CreateOptions{}); err != nil {
-				return err
+				return nil, err
 			}
-		}
-		if err := updateClusters(ctx); err != nil {
-			return err
+			if err := updateClusters(ctx); err != nil {
+				return nil, err
+			}
+			msg = p.Name
 		}
 	case constants.NamespaceType:
 		if i.status == constants.NotMigratedStatus {
 			ns := i.obj.(*cluster.Namespace)
 			ns.Mutate(constants.TC.Obj.Name, ns.ProjectName)
 			if _, err := constants.TC.Client.Namespace.Create(ns.Obj); err != nil {
-				return err
+				return nil, err
 			}
+			if err := updateClusters(ctx); err != nil {
+				return nil, err
+			}
+			msg = ns.Name
 		}
-		if err := updateClusters(ctx); err != nil {
-			return err
-		}
+
 	case constants.PRTBType:
 		if i.status == constants.NotMigratedStatus {
 			prtb := i.obj.(*cluster.ProjectRoleTemplateBinding)
 			prtb.Mutate(constants.TC.Obj.Name, prtb.ProjectName)
 			if err := constants.Lclient.ProjectRoleTemplateBindings.Create(ctx, prtb.ProjectName, prtb.Obj, nil, v1.CreateOptions{}); err != nil {
-				return err
+				return nil, err
 			}
-		}
-		if err := updateClusters(ctx); err != nil {
-			return err
+			if err := updateClusters(ctx); err != nil {
+				return nil, err
+			}
+			msg = prtb.Name
 		}
 	case constants.CRTBType:
 		if i.status == constants.NotMigratedStatus {
 			crtb := i.obj.(*cluster.ClusterRoleTemplateBinding)
 			crtb.Mutate(constants.TC)
 			if err := constants.Lclient.ClusterRoleTemplateBindings.Create(ctx, constants.TC.Obj.Name, crtb.Obj, nil, v1.CreateOptions{}); err != nil {
-				return err
+				return nil, err
 			}
+			if err := updateClusters(ctx); err != nil {
+				return nil, err
+			}
+			msg = crtb.Name
 		}
-		if err := updateClusters(ctx); err != nil {
-			return err
-		}
+
 	case constants.RepoType:
 		if i.status == constants.NotMigratedStatus {
 			repo := i.obj.(*cluster.ClusterRepo)
 			repo.Mutate()
 			if err := constants.TC.Client.ClusterRepos.Create(ctx, constants.TC.Obj.Name, repo.Obj, nil, v1.CreateOptions{}); err != nil {
-				return err
+				return nil, err
 			}
+			if err := updateClusters(ctx); err != nil {
+				return nil, err
+			}
+			msg = repo.Name
 		}
-		if err := updateClusters(ctx); err != nil {
-			return err
-		}
+
 	}
-	return nil
+	return msg, nil
 }
 
 func updateClusters(ctx context.Context) error {
